ef9a6fab26fa1c239348622c4d17ae7bca73710d
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 15:20:09 +0300

    Add navbar routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './account/login/login.component';
 
 const AccountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const ComponentsModule = () => import('./components/components.module').then(x => x.ComponentsModule);
+
 
 
 const routes: Routes = [
   {path:'Welcome',component:WelcomeComponent},
   {path:'login', component: LoginComponent },
   {path:'account',loadChildren:AccountModule},
+  {path:'components',loadChildren:ComponentsModule,},
 
   //Unrecognised routes redirect
+  { path: 'subnavbar', redirectTo: './', pathMatch: "full" },
   { path: '', redirectTo: 'welcome', pathMatch: "full" },
   { path: '**', redirectTo: 'Welcome' },
   
