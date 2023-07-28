527e2e64f7769784d7798952dfe0588fbc52f0df
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 09:35:07 +0300

    Remove material routing file

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-
 
 
 import { MatButtonModule } from '@angular/material/button';
@@ -41,16 +37,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
-  declarations: [
-  ],
-
-  imports: [
-    CommonModule,
-    FormsModule,
-  ],
-  
   exports: [
-    FormsModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
