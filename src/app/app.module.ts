b3eaef7c6898312db85e0afa5951a71a89ca0303
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 09:59:41 +0300

    import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+
 
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes } from '@angular/router'; // Import RouterModule and Routes
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CommonModule } from '@angular/common';
+import { AccountModule } from './account/account.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
-  ],
+    WelcomeComponent,
+    ],
+    
   imports: [
+    CommonModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AccountModule,
+    
   
 
   ],
   providers: [],
   
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
