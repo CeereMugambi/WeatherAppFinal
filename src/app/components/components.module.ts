11949347d974aae07635f670996113b8d2fa0ec7
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 11:30:38 +0300

    Declare and export background component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackgroundComponent } from './background/background.component';
+
+
+
+
+@NgModule({
+  declarations: [
+  
+    BackgroundComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  
+  exports:[
+    BackgroundComponent,
+  ]
+  
+    
+  
+})
+export class ComponentsModule { }
