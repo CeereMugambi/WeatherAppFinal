14a6b2fe08b8a8f2d043b2d11a0621c6a9eed531
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 13:15:38 +0300

    Add reusable sub navbar component

---
+++ src/app/components/subnavbar/subnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subnavbar',
+  templateUrl: './subnavbar.component.html',
+  styleUrls: ['./subnavbar.component.sass']
+})
+export class SubnavbarComponent {
+
+}
