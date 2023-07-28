88af6537f915d59999c6a7e463919f228aad4d5f
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-28 13:05:36 +0300

    Remove child component import

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComponentsModule } from './components.module';
-import { BackgroundComponent } from './background/background.component';
 
 const routes: Routes = [
     {
         path: '', component: ComponentsModule,
-        children:[
-           { path:'background',component:BackgroundComponent},
-        ]
       
     
     },
