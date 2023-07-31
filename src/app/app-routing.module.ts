606fed3dfe977f6d972fd806d3fc2a584248cc36
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-31 14:56:27 +0300

    Lazyload home module and add home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './helpers';
 
 const AccountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const ComponentsModule = () => import('./components/components.module').then(x => x.ComponentsModule);
+const HomeModule = ()=>import('./home/home.module').then(x=>x.HomeModule)
 
 
 
 const routes: Routes = [
   {path:'account',loadChildren:AccountModule},
   {path:'components',loadChildren:ComponentsModule,},
+  {path: 'home', loadChildren: HomeModule, canActivate: [AuthGuard] },
+
 
   //Unrecognised routes redirect
   { path: 'subnavbar', redirectTo: './', pathMatch: "full" },
