1c3d944bd2f2b7d9894129d190ab0cc20a60177a
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-07-31 14:53:44 +0300

    Declaring home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { homeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule,
