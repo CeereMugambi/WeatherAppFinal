d5b55f883fc5f0fe9e35dfe6e3b233455a8825ff
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-02 13:26:55 +0300

    Add verify-email route

--- src/app/account/account-routing.component.ts
+++ src/app/account/account-routing.component.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
 
 
 const routes: Routes = [
     
        {path: 'login',component:LoginComponent},
-       {path: 'registration',component:RegistrationComponent}
+       {path: 'registration',component:RegistrationComponent},
+       {path: 'verify-email',component:VerifyEmailComponent},
         
         
     
