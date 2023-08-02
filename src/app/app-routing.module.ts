8f62671c348accabe415dfdb037fc80c6cb00de6
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-02 14:33:30 +0300

    Add home path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './helpers';
 
 const AccountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const ComponentsModule = () => import('./components/components.module').then(x => x.ComponentsModule);
@@ -10,8 +9,8 @@ const HomeModule = ()=>import('./home/home.module').then(x=>x.HomeModule)
 
 const routes: Routes = [
   {path:'account',loadChildren:AccountModule},
-  {path:'components',loadChildren:ComponentsModule,},
-  { path: 'home', loadChildren: HomeModule, canActivate: [AuthGuard] },
+  {path:'components',loadChildren:ComponentsModule},
+  {path:'home',loadChildren:HomeModule},
 
 
   //Unrecognised routes redirect
