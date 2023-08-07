f96ca4cbff435e4ee3322a77cdc677e8f7c2c3ed
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-07 13:39:53 +0300

    Add alert route

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SubnavbarComponent } from './subnavbar/subnavbar.component';
+import { AlertComponent } from './alert/alert.component';
 
 const routes: Routes = [
-    {
-        path: 'subnavbar', component: SubnavbarComponent,
+   
+        {path: 'subnavbar', component: SubnavbarComponent},
+        {path:'alert',component:AlertComponent},
        
-    },
    
 ];
 
