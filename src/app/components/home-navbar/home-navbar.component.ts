d3e748b3b927aac7d687f195c62dd4955afcf5c8
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-07 16:38:16 +0300

    Add homenavbar logic

--- src/app/components/home-navbar/home-navbar.component.ts
+++ src/app/components/home-navbar/home-navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/services';
+import { IRole } from 'src/app/models';
+import { IAccount } from 'src/app/models';
 
 @Component({
   selector: 'app-home-navbar',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-navbar.component.sass']
 })
 export class HomeNavbarComponent {
+  Role = IRole;
+  account?: IAccount | null;
+  
+
+  constructor(private accountService: AccountService,) {
+    this.accountService.account.subscribe(x => this.account = x);
+  }
+
+
+  logout() {
+    this.accountService.logout();
+  }
+ 
+  isDropdownOpen = false;
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+  
+
+  isForecastVisible: boolean = false;
+
+  toggleForecast() {
+    this.isForecastVisible = !this.isForecastVisible;
+  }
+  
 
 }
