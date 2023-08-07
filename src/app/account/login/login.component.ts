2d6c6f190f7b3bb578246724f9af73c3a1e864de
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-07 09:17:14 +0300

    Add home route after successful login

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { first } from 'rxjs';
 import { Router,ActivatedRoute } from '@angular/router';
 import { AccountService,AlertService } from 'src/app/services';
@@ -50,7 +50,7 @@ export class LoginComponent implements OnInit {
             .subscribe({
                 next: () => {
                     // get return url from query parameters or default to home page
-                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/home';
                     this.router.navigateByUrl(returnUrl);
                 },
                 error: error => {
