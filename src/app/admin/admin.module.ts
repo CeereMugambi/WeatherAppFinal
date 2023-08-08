54acf7af4c76ffaaac5c98166334a797e0246c6e
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-08 12:54:53 +0300

    Declare and export list component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeAdminComponent } from './welcome-admin/welcome-admin.component';
-import { WelcomeRoutingModule } from '../welcome/welcome-routing.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { ComponentsModule } from '../components/components.module';
