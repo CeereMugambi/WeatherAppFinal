8524bec3007be550ec139c43ba146619e39e089a
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-08 12:09:03 +0300

    Add admin route and lazy load the module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './helpers';
+import { IRole } from './models';
 
 const AccountModule = () => import('./account/account.module').then(x => x.AccountModule);
 const ComponentsModule = () => import('./components/components.module').then(x => x.ComponentsModule);
 const HomeModule = ()=>import('./home/home.module').then(x=>x.HomeModule)
+const AdminModule = ()=>import('./admin/admin.module').then(x=>x.AdminModule)
+
 
 
 
 const routes: Routes = [
   {path:'account',loadChildren:AccountModule},
+  {path:'admin',loadChildren:AdminModule, canActivate: [AuthGuard], data: { roles: [IRole.Admin] } },
   {path:'components',loadChildren:ComponentsModule},
   {path:'home',loadChildren:HomeModule, canActivate: [AuthGuard] },
 
