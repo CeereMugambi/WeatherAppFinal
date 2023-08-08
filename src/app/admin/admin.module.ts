71194b274b7df520097d331f30fe59a4bb17e718
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-08 12:03:49 +0300

    Declare and import welcome-admin module and admin-routing module and export weelcomecomponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WelcomeAdminComponent } from './welcome-admin/welcome-admin.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeAdminComponent
+  ],
   imports: [
     CommonModule
   ]
