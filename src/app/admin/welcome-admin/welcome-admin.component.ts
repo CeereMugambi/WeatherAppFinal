56ca0053b4f916981d99948b3f935f233cffc748
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-08 11:59:06 +0300

    Welcome page for admin

---
+++ src/app/admin/welcome-admin/welcome-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-admin',
+  templateUrl: './welcome-admin.component.html',
+  styleUrls: ['./welcome-admin.component.sass']
+})
+export class WelcomeAdminComponent {
+
+}
