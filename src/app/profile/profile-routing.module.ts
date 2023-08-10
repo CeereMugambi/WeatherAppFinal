0640c32c71b9974a31598f6e181a6e6b16ad5edf
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-10 17:40:20 +0300

    Add update component path

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
+import { UpdateComponent } from './update/update.component';
 
 const routes: Routes = [
-    {path:'details',component:DetailsComponent}
+    {path:'details',component:DetailsComponent},
+    {path: 'update',component:UpdateComponent}
     
 ];
 
