d398efe5261c147770c25573b21fcd4d4f9c8204
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-10 12:07:06 +0300

    Add edit-admin route path

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeAdminComponent } from './welcome-admin/welcome-admin.component';
 import { ListComponent } from './list/list.component';
+import { EditAdminComponent } from './edit-admin/edit-admin.component';
 
 
 const routes: Routes = [
     
        {path: 'welcome-admin',component:WelcomeAdminComponent},
-       {path:'list',component:ListComponent}
+       {path:'list',component:ListComponent},
+       {path:'add',component:EditAdminComponent},
+
+       
+
+       
+
 
       
         
