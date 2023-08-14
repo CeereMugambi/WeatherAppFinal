fba0a0f57d9f9ba432f5ca38af972cfa37457200
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-14 16:09:25 +0300

    Update logic

--- src/app/helpers/app.initializer.ts
+++ src/app/helpers/app.initializer.ts
@@ -1,6 +1,7 @@
 import { catchError, of } from 'rxjs';
 
 import { AccountService } from '../services';
+
 export function appInitializer(accountService: AccountService) {
     return () => accountService.refreshToken()
         .pipe(
