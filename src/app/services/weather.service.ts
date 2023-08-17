0ea6fc83c48e610b798c2b763c3d444c834a05e4
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-17 14:56:28 +0300

    update service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IweatherData } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-}
+  getWeatherData(cityName: string): Observable<IweatherData> {
+    return this.http.get<IweatherData>(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set(environment.weatherApiXRapidAPIHostHeaderName, environment.weatherApiXRapidAPIHostHeaderValue)
+      .set(environment.weatherApiXRapidAPIKeyHeaderName, environment.weatherApiXRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('units', 'metric')
+      .set('mode', 'json')
+    })
+  }
+  }
