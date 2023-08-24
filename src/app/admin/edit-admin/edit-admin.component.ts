bbf7c9809bf85227246174a09be5f5ae8bb4ee16
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-24 11:28:19 +0300

    Update route

--- src/app/admin/edit-admin/edit-admin.component.ts
+++ src/app/admin/edit-admin/edit-admin.component.ts
@@ -1,15 +1,13 @@
 import { Component,} from '@angular/core';
 import { Router } from '@angular/router';
 
-
-
 @Component({ 
   templateUrl: 'edit-admin.component.html',
   styleUrls: ['./edit-admin.component.sass'],
  })
 
 export class EditAdminComponent {
-    title!: string;
+    title = 'Create Account';
 
     constructor(private router: Router) {}
     
