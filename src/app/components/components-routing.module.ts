479b611834e22bbd865633257ff2c9034e032e74
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2023-08-28 10:11:01 +0300

    Delete weathercard component

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SubnavbarComponent } from './subnavbar/subnavbar.component';
 import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 const routes: Routes = [
    
         {path: 'subnavbar', component: SubnavbarComponent},
         {path:'home-navbar',component:HomeNavbarComponent},
-        {path:'weathercard',component:WeatherCardComponent}
        
    
 ];
